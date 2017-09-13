a9f492eb62182f74805e7d8d1e71dcf4903142e9
linkcoding <kenny.lin.91@gmail.com>
2017-09-13 17:52:20 -0400

    created basic allergy list

---
+++ src/components/allergy.js
@@ -0,0 +1,6 @@
+const allergy = {
+  dairy: ["Butter", "Cheese", "Cream", "Milk", "Yogurt", "Ice cream", "Pudding"],
+  treeNuts: ["Almond", "Almond paste", "Anacardium nuts", "Artificial nuts", "Beech nut", "Brazil nut", "Bush nut", "Butternut", "Caponata", "Cashew", "Chinquapin", "Coconut", "Filbert", "Gianduja", "Ginko nut", "Hazelnut", "Heartnut", "Hickory nut", "Indian nut", "Lichee nut", "Lychee nut", "Macadamia nut", "Mandelonas", "Marzipan", "Mashuga nuts", "Nangai nuts", "Nougat", "Nu-Nuts", "Nut meal", "Nutella ", "Nutmeat", "Nut paste", "Nut pieces", "Pecan", "Pili nut", "Pine nut", "Pinon nut", "Pistachio", "Pralines", "Shea nut", "Sheanut", "Walnut"],
+  peanuts: ["Peanut"],
+  soy: ["Edamame", "Miso", "Natto", "Shoyu", "Soy", "Soya", "Soybean", "TVP", "Tamari", "Tempeh", "Textured vegetable protein", "Tofu"]
+}
