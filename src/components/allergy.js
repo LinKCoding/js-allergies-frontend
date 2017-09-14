453013af0c9afb33cd07322e36b4700c17490924
linkcoding <kenny.lin.91@gmail.com>
2017-09-14 12:56:28 -0400

    fixed common allergies

--- src/components/allergy.js
+++ src/components/allergy.js
@@ -1,6 +1,6 @@
 const ALLERGEN = {
-  dairy: ["Butter", "Cheese", "Cream", "Milk", "Yogurt", "Ice cream", "Pudding"],
-  treeNuts: ["Almond", "Almond paste", "Butternut", "Caponata", "Cashew", "Chinquapin", "Coconut", "Filbert", "Gianduja", "Hazelnut", "Heartnut", "Mandelonas", "Marzipan", "Nougat", "Nu-Nuts", "Nut meal", "Nutella ", "Nutmeat", "Nut paste", "Nut pieces", "Pecan", "Pistachio", "Pralines", "Sheanut", "Walnut", "Nut"],
+  milk: ["Butter", "Cheese", "Cream", "Milk", "Yogurt", "Ice cream", "Pudding"],
+  treenuts: ["Almond", "Almond paste", "Butternut", "Caponata", "Cashew", "Chinquapin", "Coconut", "Filbert", "Gianduja", "Hazelnut", "Heartnut", "Mandelonas", "Marzipan", "Nougat", "Nu-Nuts", "Nut meal", "Nutella ", "Nutmeat", "Nut paste", "Nut pieces", "Pecan", "Pistachio", "Pralines", "Sheanut", "Walnut", "Nut"],
   peanuts: ["Peanut"],
   soy: ["Edamame", "Miso", "Natto", "Shoyu", "Soy", "Soya", "Soybean", "TVP", "Tamari", "Tempeh", "Textured vegetable protein", "Tofu"],
   gluten: ["Wheat", "Wheatberries", "Durum", "Emmer", "Semolina", "Spelt", "Farina", "Farro", "Graham", "Kamut", "Khorasan", "Einkorn", "Rye", "Barley", "Triticale", "Malt", "Yeast", "Starch"],
