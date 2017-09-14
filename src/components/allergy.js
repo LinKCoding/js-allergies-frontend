d8a51c29838dc0eb5ed9631e389a14cd27f6d0b2
linkcoding <kenny.lin.91@gmail.com>
2017-09-14 08:59:41 -0400

    added list of allergies

--- src/components/allergy.js
+++ src/components/allergy.js
@@ -1,6 +1,9 @@
 const allergy = {
   dairy: ["Butter", "Cheese", "Cream", "Milk", "Yogurt", "Ice cream", "Pudding"],
-  treeNuts: ["Almond", "Almond paste", "Anacardium nuts", "Artificial nuts", "Beech nut", "Brazil nut", "Bush nut", "Butternut", "Caponata", "Cashew", "Chinquapin", "Coconut", "Filbert", "Gianduja", "Ginko nut", "Hazelnut", "Heartnut", "Hickory nut", "Indian nut", "Lichee nut", "Lychee nut", "Macadamia nut", "Mandelonas", "Marzipan", "Mashuga nuts", "Nangai nuts", "Nougat", "Nu-Nuts", "Nut meal", "Nutella ", "Nutmeat", "Nut paste", "Nut pieces", "Pecan", "Pili nut", "Pine nut", "Pinon nut", "Pistachio", "Pralines", "Shea nut", "Sheanut", "Walnut"],
+  treeNuts: ["Almond", "Almond paste", "Butternut", "Caponata", "Cashew", "Chinquapin", "Coconut", "Filbert", "Gianduja", "Hazelnut", "Heartnut", "Mandelonas", "Marzipan", "Nougat", "Nu-Nuts", "Nut meal", "Nutella ", "Nutmeat", "Nut paste", "Nut pieces", "Pecan", "Pistachio", "Pralines", "Sheanut", "Walnut", "Nut"],
   peanuts: ["Peanut"],
-  soy: ["Edamame", "Miso", "Natto", "Shoyu", "Soy", "Soya", "Soybean", "TVP", "Tamari", "Tempeh", "Textured vegetable protein", "Tofu"]
+  soy: ["Edamame", "Miso", "Natto", "Shoyu", "Soy", "Soya", "Soybean", "TVP", "Tamari", "Tempeh", "Textured vegetable protein", "Tofu"],
+  gluten: ["Wheat", "Wheatberries", "Durum", "Emmer", "Semolina", "Spelt", "Farina", "Farro", "Graham", "Kamut", "Khorasan", "Einkorn", "Rye", "Barley", "Triticale", "Malt", "Yeast", "Starch"],
+  fish: ["Anchovies", "Bass", "Catfish", "Cod", "Flounder", "Grouper", "Haddock", "Hake", "Halibut", "Herring", "Mahi mahi", "Perch", "Pike", "Pollock", "Salmon", "Scrod", "Sole", "Snapper", "Swordfish", "Tilapia", "Trout", "Tuna", "Fish"],
+  shellfish: ["Barnacle", "Crab", "Crawfish", "Crawdad", "Crayfish", "Ecrevisse", "Krill", "Lobster", "Langouste", "Langoustine", "Moreton bay bugs", "Scampi", "Tomalley", "Prawns", "Shrimp", "Crevette", "Scampi", "Abalone", "Clams", "Cherrystone", "Geoduck", "Littleneck", "Pismo", "Quahog", "Cockle", "Cuttlefish", "Limpet", "Lapas", "Opihi", "Mussels", "Octopus", "Oysters", "Periwinkle", "Sea cucumber", "Sea", "Urchin", "Scallops", "Snails", "Escargot", "Squid", "Calamari", "Whelk", "Turban shell"]
 }
